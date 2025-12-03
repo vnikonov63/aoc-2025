c0a80192b5bb3d3e9edfa8ce6d858198bbd103df
Vasilii Nikonov <nikonvasilii@gmail.com>
2025-12-03 05:32:36 -0800

    Finished Day 3 Part 2
    Solved via a Monotonic Stack. Remebered CSE 130 stuff using a fold in
    here. Got confused with the &'s and just variables. The `.trim()` is
    really the perpetrator, I should be very carefull with it.

--- src/day03.rs
+++ src/day03.rs
@@ -26,12 +26,30 @@ pub fn part1<R: BufRead>(mut input: R) -> String {
 
 pub fn part2<R: BufRead>(mut input: R) -> String {
     let mut buf = String::new();
-    let mut result = 0;
+    let mut result: u64 = 0;
 
     while let Ok(num_bytes_read) = input.read_line(&mut buf) {
         if num_bytes_read == 0 {
             break;
         }
+        let mut to_remove = buf.trim().len() - 12;
+        let mut stack: Vec<u8> = Vec::new();
+
+        for ch in buf.trim().chars().map(|x| x.to_digit(10).unwrap() as u8) {
+            while let Some(&last) = stack.last() {
+                if last < ch && to_remove != 0 {
+                    stack.pop();
+                    to_remove -= 1;
+                } else {
+                    break;
+                }
+            }
+            stack.push(ch);
+        }
+
+        stack.truncate(12);
+        result += stack.iter().fold(0u64, |acc, &ch| acc * 10 + ch as u64);
+
         buf.clear();
     }
 
@@ -49,5 +67,13 @@ mod tests {
         let cursor = Cursor::new(input.as_bytes());
         let result = part1(cursor);
         assert_eq!(result, "357");
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "987654321111111\n811111111111119\n34234234234278\n818181911112111";
+        let cursor = Cursor::new(input.as_bytes());
+        let result = part2(cursor);
+        assert_eq!(result, "3121910778619");
     }
 }
